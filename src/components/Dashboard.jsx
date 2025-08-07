44df6fb35433c1d9961edc2266940fc3a18951ec
sarojsubedi <sarojsubedi@pm.me>
2025-08-07 20:55:32 +0545

    styles: changed the border and font weight

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,15 +2,14 @@ const Dashboard = () => {
   const balance = 0;
   return (
     <>
-      <div className="flex flex-col gap-8 border p-4 min-w-3xl">
+      <div className="flex flex-col gap-8 p-4 min-w-3xl">
         <div>
           <h2 className="text-3xl font-bold">Dashboard</h2>
           <p className="text-xs p-4 pl-0">
             Your financial overview at a glance.
           </p>
-
-          <h3 className="text-xl font-bold">Income vs. Expense</h3>
-          <div className="flex flex-col border p-4">
+          <h3 className="text-xl font-bold mb-4">Income vs. Expense</h3>
+          <div className="flex flex-col p-4 border border-gray-300 rounded-2xl">
             Last 6 months
             <span className="font-bold">Rs. {balance}</span>
             <img
@@ -21,8 +20,8 @@ const Dashboard = () => {
           </div>
         </div>
         <div>
-          <h3>Spending by Category</h3>
-          <div className="flex flex-col p-4">
+          <h3 className="text-xl font-bold">Spending by Category</h3>
+          <div className="flex flex-col mt-4 p-4 border border-gray-300 rounded-2xl">
             <img
               src="https://placehold.co/400x200"
               alt="chart"
@@ -31,8 +30,8 @@ const Dashboard = () => {
           </div>
         </div>
         <div>
-          <h3>Savings Goals</h3>
-          <div className="flex flex-col p-4">
+          <h3 className="text-xl font-bold">Savings Goals</h3>
+          <div className="flex flex-col mt-4 p-4 border border-gray-300 rounded-2xl">
             <img
               src="https://placehold.co/400x200"
               alt="chart"
